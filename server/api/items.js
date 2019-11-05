415477ecc523aea724fa54bdcf6fe0a03291d527
Josh Wintraub <joshbw4@gmail.com>
2019-11-05 12:40:46 -0500

    Item routes

---
+++ server/api/items.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {Item} = require('../db/models');
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const item = await Item.findByPk(req.params.id);
+    res.send(item);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/', async (req, res, next) => {
+  try {
+    const items = await Item.findAll();
+    res.send(items);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
