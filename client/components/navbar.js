41a4c8cc2ce056315d4cf791f89048198ca9424f
Maksim Pesetski <maksim.pesetski@gmail.com>
2019-11-06 16:32:47 -0500

    code review ver

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -22,7 +22,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           {/* The navbar will show these links after you log in */}
           <Link to="/">
             <img
-              src="https://cdn5.vectorstock.com/i/1000x1000/60/34/whiskey-barrel-premium-quality-wheat-wreath-flat-vector-13806034.jpg"
+              src="https://www.uncommongoods.com/images/items/23900/23960_1_640px.jpg"
               width={50}
             />
           </Link>
@@ -42,8 +42,8 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           {/* The navbar will show these links before you log in */}
           <Link to="/">
             <img
-              src="https://cdn5.vectorstock.com/i/1000x1000/60/34/whiskey-barrel-premium-quality-wheat-wreath-flat-vector-13806034.jpg"
-              width={50}
+              src="https://www.uncommongoods.com/images/items/23900/23960_1_640px.jpg"
+              width={75}
             />
           </Link>
           <Link to="/catalog">Catalog</Link>
