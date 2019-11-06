87eacd8198584cff37025b97eeee1af2334be705
Maksim Pesetski <maksim.pesetski@gmail.com>
2019-11-06 16:32:47 -0500

    code review ver

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
-import {logout} from '../store';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { logout } from '../store';
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+const Navbar = ({ handleClick, isLoggedIn }) => (
   <div>
     <div className="plate">
       <p className="script">
@@ -38,25 +38,25 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           </a>
         </div>
       ) : (
-        <div id="navBar">
-          {/* The navbar will show these links before you log in */}
-          <Link to="/">
-            <img
-              src="https://www.uncommongoods.com/images/items/23900/23960_1_640px.jpg"
-              width={75}
-            />
-          </Link>
-          <Link to="/catalog">Catalog</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/checkout">
-            <img
-              src="https://pngriver.com/wp-content/uploads/2018/04/Download-Shopping-Cart-Logo-Png-Image-78016-For-Designing-Projects.png"
-              width={50}
-            />
-          </Link>
-        </div>
-      )}
+          <div id="navBar">
+            {/* The navbar will show these links before you log in */}
+            <Link to="/">
+              <img
+                src="https://www.uncommongoods.com/images/items/23900/23960_1_640px.jpg"
+                width={75}
+              />
+            </Link>
+            <Link to="/catalog">Catalog</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/checkout">
+              <img
+                src="https://pngriver.com/wp-content/uploads/2018/04/Download-Shopping-Cart-Logo-Png-Image-78016-For-Designing-Projects.png"
+                width={50}
+              />
+            </Link>
+          </div>
+        )}
     </nav>
     <hr />
   </div>
