a0aacf557f01283959d81520211d263ecdcf85f0
Danny Gutmann <dannycgutmann@gmail.com>
2019-11-11 15:59:13 -0500

    user routes are secured

--- server/api/users.js
+++ server/api/users.js
@@ -4,11 +4,16 @@ module.exports = router;
 
 router.get('/', async (req, res, next) => {
   try {
-    res.send(
-      await User.findAll({
-        attributes: ['id', 'email', 'firstName', 'lastName', 'flag']
-      })
-    );
+    console.log(req.user.flag);
+    if (req.user.flag) {
+      res.send(
+        await User.findAll({
+          attributes: ['id', 'email', 'firstName', 'lastName', 'flag']
+        })
+      );
+    } else {
+      res.redirect('/');
+    }
   } catch (error) {
     next(error);
   }
@@ -16,13 +21,15 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   try {
-    res.send(
-      await User.findByPk(req.params.id, {
-        where: {
-          attributes: ['id', 'email', 'firstName', 'lastName', 'flag']
-        }
-      })
-    );
+    if (req.user.id === req.params.id || req.user.flag === 1) {
+      res.send(
+        await User.findByPk(req.params.id, {
+          where: {
+            attributes: ['id', 'email', 'firstName', 'lastName', 'flag']
+          }
+        })
+      );
+    }
   } catch (error) {
     next(error);
   }
