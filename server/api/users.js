8f3ad68371cb456f1847b2ab7ab5cd1db44615a1
Maksim Pesetski <maksim.pesetski@gmail.com>
2019-11-11 18:16:23 -0500

    working sign up

--- server/api/users.js
+++ server/api/users.js
@@ -1,28 +1,33 @@
 const router = require('express').Router();
 const {User} = require('../db/models');
 module.exports = router;
-
 router.get('/', async (req, res, next) => {
   try {
-    res.send(
-      await User.findAll({
-        attributes: ['id', 'email', 'firstName', 'lastName', 'flag']
-      })
-    );
+    console.log(req.user.flag);
+    if (req.user.flag) {
+      res.send(
+        await User.findAll({
+          attributes: ['id', 'email', 'firstName', 'lastName', 'flag']
+        })
+      );
+    } else {
+      res.redirect('/');
+    }
   } catch (error) {
     next(error);
   }
 });
-
 router.get('/:id', async (req, res, next) => {
   try {
-    res.send(
-      await User.findByPk(req.params.id, {
-        where: {
-          attributes: ['id', 'email', 'firstName', 'lastName', 'flag']
-        }
-      })
-    );
+    if (req.user.id === req.params.id || req.user.flag === 1) {
+      res.send(
+        await User.findByPk(req.params.id, {
+          where: {
+            attributes: ['id', 'email', 'firstName', 'lastName', 'flag']
+          }
+        })
+      );
+    }
   } catch (error) {
     next(error);
   }
